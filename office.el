c6409e00c8009c7af30659a0f66e70a377abb27f
Javier Novoa C <jstitch@gmail.com>
2017-09-29 19:45:59 -0500

    Let's try some spacemacs theme...

--- office.el
+++ office.el
@@ -31,6 +31,14 @@
  'org-babel-load-languages
  '((python . t) (sh . t)))
 
+;; set sizes here to stop spacemacs theme resizing these
+(set-face-attribute 'org-level-1 nil :height 1.0)
+(set-face-attribute 'org-level-2 nil :height 1.0)
+(set-face-attribute 'org-level-3 nil :height 1.0)
+(set-face-attribute 'org-scheduled-today nil :height 1.0)
+(set-face-attribute 'org-agenda-date-today nil :height 1.1)
+(set-face-attribute 'org-table nil :foreground "#008787")
+
 
 ;; SCRUM
 (load-user-file "scrum.el")
--- tweaks.el
+++ tweaks.el
@@ -1,3 +1,19 @@
+;; Spacemacs theme
+(use-package spacemacs-theme
+  :ensure t
+  :init
+  (load-theme 'spacemacs-dark t)
+  (setq spacemacs-theme-org-agenda-height nil)
+  (setq spacemacs-theme-org-height nil))
+(use-package spaceline
+  :demand t
+  :init
+  (setq powerline-default-separator 'arrow-fade)
+  :config
+  (require 'spaceline-config)
+  (spaceline-spacemacs-theme))
+
+
 ;; Multiple cursors
 (use-package multiple-cursors
   :load-path "/usr/share/emacs/site-lisp/multiple-cursors"
