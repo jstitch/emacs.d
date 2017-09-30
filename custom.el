3f82f95fe2b2a7051f7ef6730b3e6682a1560531
Javier Novoa C <jstitch@gmail.com>
2017-09-30 10:06:47 -0500

    Rearrange custom and editor_custom.
    I want the light version to load even faster.

--- custom.el
+++ custom.el
@@ -25,18 +25,6 @@
   :ensure t
 )
 
-;; recent files
-(use-package recentf
-  :defer t
-  :ensure t
-  :bind ("s-r" . recentf-open-files)
-  :config
-  (recentf-mode 1)
-  (setq recentf-max-menu-items 25)
-  (add-to-list 'recentf-exclude no-littering-var-directory)
-  (add-to-list 'recentf-exclude no-littering-etc-directory)
-)
-
 ;; create parent subdirectories before saving
 (add-hook 'before-save-hook
           '(lambda ()
@@ -49,22 +37,6 @@
 (defun display-ansi-colors ()
   (interactive)
   (ansi-color-apply-on-region (point-min) (point-max)))
-
-;; ibuffer
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-
-;; ace-jump
-(global-set-key (kbd "C-c u") 'ace-jump-mode)
-
-;; ;; persistent-scratch
-;; (use-package persistent-scratch
-;;   :config
-;;   (persistent-scratch-setup-default))
-
-(use-package info-look)
-
-;; stop on subwords (camelCase words)
-(global-subword-mode 1)
 
 ;; make typing NOT delete/overwrites selected text
 (delete-selection-mode 0)
@@ -95,11 +67,6 @@
 ;;(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-12"))
 ;;(set-face-attribute 'default nil :font "Courier New-12")
 
-;; Transparency
-;;;;(set-frame-parameter (selected-frame) 'alpha '(<active> [<inactive>]))
-(set-frame-parameter (selected-frame) 'alpha '(85 75))
-(add-to-list 'default-frame-alist '(alpha 85 75))
-
 ;; Close current window, fast
 (global-set-key (kbd "s-0") 'delete-window)
 ;; One window, fast
@@ -121,19 +88,6 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 
-;; empty lines at bottom indicator
-;; extra whitespace in red
-(add-hook 'prog-mode-hook
-          (lambda ()
-            (setq show-trailing-whitespace t)
-            (setq indicate-empty-lines t)))
-
-;; current line
-(add-hook 'prog-mode-hook 'hl-line-mode)
-
-;; Copy to clipboard
-(load-file "~/.emacs.d/site-lisp/xclip.el")
-
 ;; indent with C-tab
 (global-set-key (kbd "s-<tab>") 'indent-region)
 
@@ -141,36 +95,6 @@
 (global-set-key (kbd "s-c") 'comment-region)
 ;; uncomment region with C-c C-u
 (global-set-key (kbd "s-u") 'uncomment-region)
-
-;; find file in repo
-(use-package find-file-in-repository
-  :commands find-file-in-repository
-  :bind ("C-x f" . find-file-in-repository)
-  :ensure t
-)
-
-;; idle-highlight-mode
-(use-package idle-highlight-mode
-  :ensure t
-  :commands idle-highlight-mode
-)
-
-;; today's date
-;; from http://emacswiki.org/emacs/InsertingTodaysDate
-(defun insert-todays-date (arg)
-  (interactive "P")
-  (insert (if arg
-              (format-time-string "%d-%m-%Y")
-            (format-time-string "%Y-%m-%d"))))
-
-;; Color-Theme
-(use-package color-theme
-  :defer t
-)
-;; Github-theme
-(use-package github-theme
-  :defer t
-)
 
 ;; CSV-mode
 (use-package csv-mode
--- editor_custom.el
+++ editor_custom.el
@@ -1,9 +1,85 @@
+;; ;; persistent-scratch
+;; (use-package persistent-scratch
+;;   :config
+;;   (persistent-scratch-setup-default))
+
+;; empty lines at bottom indicator
+;; extra whitespace in red
+(add-hook 'prog-mode-hook
+          (lambda ()
+            (setq show-trailing-whitespace t)
+            (setq indicate-empty-lines t)))
+
+;; current line
+(add-hook 'prog-mode-hook 'hl-line-mode)
+
+;; Copy to clipboard
+(load-file "~/.emacs.d/site-lisp/xclip.el")
+
+;; stop on subwords (camelCase words)
+(global-subword-mode 1)
+
+;; Transparency
+;;;;(set-frame-parameter (selected-frame) 'alpha '(<active> [<inactive>]))
+(set-frame-parameter (selected-frame) 'alpha '(85 75))
+(add-to-list 'default-frame-alist '(alpha 85 75))
+
+;; find file in repo
+(use-package find-file-in-repository
+  :commands find-file-in-repository
+  :bind ("C-x f" . find-file-in-repository)
+  :ensure t
+)
+
+;; idle-highlight-mode
+(use-package idle-highlight-mode
+  :ensure t
+  :commands idle-highlight-mode
+)
+
+;; today's date
+;; from http://emacswiki.org/emacs/InsertingTodaysDate
+(defun insert-todays-date (arg)
+  (interactive "P")
+  (insert (if arg
+              (format-time-string "%d-%m-%Y")
+            (format-time-string "%Y-%m-%d"))))
+
+;; Color-Theme
+(use-package color-theme
+  :defer t
+)
+;; Github-theme
+(use-package github-theme
+  :defer t
+)
+
+(use-package info-look)
+
+;; ibuffer
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;; ace-jump
+(global-set-key (kbd "C-c u") 'ace-jump-mode)
+
 ;; Emacs window config
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (windmove-default-keybindings 'shift)
 (show-paren-mode t)
+
+;; recent files
+(use-package recentf
+  :defer t
+  :ensure t
+  :bind ("s-r" . recentf-open-files)
+  :config
+  (recentf-mode 1)
+  (setq recentf-max-menu-items 25)
+  (add-to-list 'recentf-exclude no-littering-var-directory)
+  (add-to-list 'recentf-exclude no-littering-etc-directory)
+)
 
 (use-package exec-path-from-shell
   :defer t
