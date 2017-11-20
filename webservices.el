ba12eabe85625da2ea64b49ebbec19965845c068
jstitch <jstitch@gmail.com>
2017-11-19 21:01:06 -0600

    w3m package

--- webservices.el
+++ webservices.el
@@ -1,4 +1,6 @@
 ;; W3M mode
+(use-package w3m
+  :ensure t)
 ;;  (use-package w3m-load)
 (setq browse-url-browser-function 'w3m-browse-url)
 (autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
