be261e741d0d4b3320866c6dee43cb5e5e59eb80
Javier Novoa <jstitch@gmail.com>
2020-06-11 21:44:14 -0500

    EXWM improvements

--- exwm.el
+++ exwm.el
@@ -1,6 +1,8 @@
 (load-user-file "editor_custom.el")
 
 (require 'exwm)
+(require 'exwm-systemtray)
+(exwm-systemtray-enable)
 (require 'exwm-config)
 (exwm-config-default)
 
@@ -12,5 +14,18 @@
 (global-set-key (kbd "C-s-2") 'split-window-below)
 ;; Two windows (vert split), fast
 (global-set-key (kbd "C-s-3") 'split-window-right)
+;; Lock screen (screensaver)
+(defun screensaver ()
+  "Activate screensaver."
+  (interactive)
+  (shell-command "xscreensaver-command -activate"))
+(global-set-key (kbd "C-M-S-l") 'screensaver)
+
+(display-time-mode)
+(display-battery-mode)
 
 (eshell)
+(kill-buffer "*About GNU Emacs*")
+
+(provide 'exwm)
+;;; exwm ends here
