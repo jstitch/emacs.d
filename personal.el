82c2b56f39b7914b3f233e30fd3a648698881b57
Javier Novoa <jstitch@gmail.com>
2020-06-11 21:47:33 -0500

    Add perrsonal info: work email address

--- personal.el
+++ personal.el
@@ -1,3 +1,4 @@
 ;; (setq user-mail-address "jstitch@invernalia.homelinux.net")
-(setq user-mail-address "jstitch@gmail.com")
+; (setq user-mail-address "jstitch@gmail.com") }
+(setq user-mail-address "j.novoa@podemos.mx")
 (setq user-full-name "Javier Novoa C.")
