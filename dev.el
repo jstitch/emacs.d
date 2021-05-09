3dab60f1ad2c9b46e8d9213e542270011ecb71b9
Javier Novoa <jstitch@gmail.com>
2021-05-08 23:03:51 -0500

    company mode

--- dev.el
+++ dev.el
@@ -52,6 +52,25 @@
   :defer t)
 (setq yasnippet-snippets-dir "/home/jstitch/.emacs.d/etc/yasnippet/yasnippet-snippets/snippets")
 
+(use-package company
+  :ensure
+  :custom
+  (company-idle-delay 0.5) ;; how long to wait until popup
+  ;; (company-begin-commands nil) ;; uncomment to disable popup
+  :bind
+  (:map company-active-map
+	      ("C-n". company-select-next)
+	      ("C-p". company-select-previous)
+	      ("M-<". company-select-first)
+	      ("M->". company-select-last))
+  (:map company-mode-map
+	    ("<tab>". tab-indent-or-complete)
+	    ("TAB". tab-indent-or-complete)))
+
+(defun company-yasnippet-or-completion ()
+  (interactive)
+  (or (do-yas-expand)
+      (company-complete-common)))
 ;;;
 ;;--------------------------------------------------------------------
 ;; ;; Lines enabling gnuplot-mode
