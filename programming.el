d57fe624ec986b3076128fba1d662f93d5c5f9a6
Javier Novoa <jstitch@gmail.com>
2021-05-08 23:04:28 -0500

    rustic and lsp mode for Rust development instead of rust mode

--- programming.el
+++ programming.el
@@ -186,7 +186,59 @@
 
 
 ;; Rust
-(use-package rust-mode
+(use-package rustic
+  :ensure
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
+
+  ;; comment to disable rustfmt on save
+  (setq rustic-format-on-save t)
+  (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
+
+(defun rk/rustic-mode-hook ()
+  ;; so that run C-c C-c C-r works without having to confirm
+  (setq-local buffer-save-without-query t))
+
+(use-package lsp-mode
+  :ensure
+  :commands lsp
+  ;; :config
+  ;; (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  (lsp-rust-analyzer-server-display-inlay-hints t))
+
+(setq lsp-rust-analyzer-server-display-inlay-hints t)
+
+(use-package lsp-ui
+  :ensure
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil))
+
+;; (use-package flycheck-rust
+;;   :defer t
+;;   :ensure t)
+;; (add-hook 'rust-mode-hook 'cargo-minor-mode)
+
+
   :defer t
   :ensure t)
 
